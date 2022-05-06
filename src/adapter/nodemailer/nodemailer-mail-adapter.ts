dcc877fe4e560ee3e04b9b860eaa0dcf3e1e42eb
gustavo henrique <pcasemirog@gmail.com>
2022-05-06 12:13:10 -0300

    first commit

---
+++ src/adapter/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "ad3c0c9cfc071b",
+      pass: "32ca243cdb70c4"
+    }
+  });
+
+export class NodemailerMailAdapter implements MailAdapter{
+    async sendMail({subject,body}: SendMailData){
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Gustavo Henrique <pcasemiro@gmail.com>',
+            subject,
+            html: body,
+        });
+    };
+
+}
